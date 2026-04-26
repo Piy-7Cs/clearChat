7c49337cb08d5882e4d16cd219f9c77569ebbc28
redCat <user1299@duck.com>
2026-04-26 20:04:29 +0530

    minor fix

--- app/models/membership.py
+++ app/models/membership.py
@@ -5,8 +5,8 @@ from app.db.base import Base
 class Membership(Base):
     __tablename__ = "memberships"
      
-    user_id = Column(String, ForeignKey="users.id", primary_key=True)
-    room_id = Column(String, ForeignKey="rooms.id",primary_key=True)
+    user_id = Column(String, ForeignKey("users.id"), primary_key=True)
+    room_id = Column(String, ForeignKey("rooms.id"), primary_key=True)
 
     role = Column(String, default="member") #member | owner | moderator
     status = Column(String, default="active") #active | pending | banned
